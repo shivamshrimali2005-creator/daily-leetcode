dd88ebfe458b67ce05c74677505083c28dd992ee
SHIVAM SHRIMALI <shivamshrimali2005@gmail.com>
2026-07-21 23:10:19 +0530

    Implement defangIPaddr function to defang IP addresses

---
+++ String/Defanging an IP Address.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        
+        string ans = "";
+        int n = address.size();
+
+        for(int i=0;i<n;i++){
+            if(address[i] == '.'){
+                ans += '[';
+                ans += '.';
+                ans += ']';
+            }
+            else ans += address[i];
+        }
+
+        return ans;
+    }
+};
